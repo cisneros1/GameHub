c08ccd3e215520d4eb32230c3110455986756ab3
cisneros1 <briancis@buffalo.edu>
2024-01-14 21:20:46 -0500

    display platform icons

--- src/components/hooks/useGames.ts
+++ src/components/hooks/useGames.ts
@@ -3,10 +3,19 @@ import {useEffect, useState} from "react";
 import apiClient from "../../services/api-client.ts";
 import {CanceledError} from "axios";
 
+export interface Platform {
+    id: number
+    name: string
+    slug: string
+}
+
 export interface Game {
     id: number
     name: string
     background_image: string
+    // Parent platform is not a platform array which is should be
+    // instead it is an array of objects where each object has a property called platform
+    parent_platforms: {platform: Platform}[]
 }
 
 interface FetchGamesResponse {
