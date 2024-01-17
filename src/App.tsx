774e2ebc71cd9e1f28935a08974a61cea98fa671
cisneros1 <briancis@buffalo.edu>
2024-01-17 11:33:11 -0500

    built search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {Grid, GridItem, HStack, Show} from '@chakra-ui/react'
+import {Box, Flex, Grid, GridItem, HStack, Show} from '@chakra-ui/react'
 import './App.css'
 import NavBar from "./components/NavBar.tsx";
 import GameGrid from "./components/GameGrid.tsx";
@@ -41,11 +41,15 @@ function App() {
                     </GridItem>
                 </Show>
                 <GridItem area={'main'}>
-                    <HStack spacing={5} padding={2} marginBottom={5}>
-                        <PlatformSelector selectedPlatform={gameQuery.platform}
-                                          onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}/>
-                        <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
-                    </HStack>
+                    <Flex padding={2} marginBottom={5}>
+                        <Box marginRight={5}>
+                            <PlatformSelector selectedPlatform={gameQuery.platform}
+                                              onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}/>
+                        </Box>
+
+                        <SortSelector sortOrder={gameQuery.sortOrder}
+                                      onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
+                    </Flex>
                     <GameGrid gameQuery={gameQuery}/>
                 </GridItem>
 
