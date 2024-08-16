31a9a1796b32e04fcba7182f957efbb95457dc02
Brian Cisneros <briancis@buffalo.edu>
2024-08-16 18:50:48 -0400

    added home nav through logo

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,16 +2,20 @@ import {Input, InputGroup, InputLeftElement} from "@chakra-ui/react";
 import {BsSearch} from "react-icons/all";
 import {useRef} from "react";
 import useGameQueryStore from "../store.ts";
+import {useNavigate} from "react-router-dom";
 
 
 const SearchInput = () => {
     const ref = useRef<HTMLInputElement>(null)
     const setSearchText = useGameQueryStore(selector => selector.setSearchText)
+    const navigate = useNavigate()
 
     return (
         <form onSubmit={(event) => {
             event.preventDefault()
-            if (ref.current) setSearchText(ref.current.value)
+            if (ref.current)
+                setSearchText(ref.current.value)
+                navigate('/')
         }}>
             <InputGroup>
                 <InputLeftElement children={<BsSearch/>}/>
